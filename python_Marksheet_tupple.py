b95fbd0f020e5804c34467c7ca57724e7ccecdc5
Kaushik Jain <66528239+kaushik73@users.noreply.github.com>
2021-05-24 22:27:38 +0530

    Add files via upload

---
+++ python_Marksheet_tupple.py
@@ -0,0 +1,68 @@
+def add(a,b):
+ sum=a+b
+ return sum
+
+print(add(6,5))
+#----------------------------------------------------------------------------#
+# print("YOUR 3RD SEMESTER RESULT :")
+# marks = {}
+# subject = int(5)
+# marks = {}
+# def marksheet():
+#  for i in  range(0,subject+1 ): 
+#   marks[i]= input(f"Enter marks of sub{i}:")
+# marksheet() 
+# print("_________________________________________________")
+# print("YOUR 3RD SEMESTER RESULT :")
+# print(marks)
+
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+#PROGRAM TO USE SOME FUNCTIONS OF DICTIONARY:-
+# subject = int(input("Enter the number of Subject : "))
+# marks = {}
+
+# def marksheet():
+#  for i in  range(0,subject ): 
+
+#   marks[i]= input(f"Enter marks of sub{i+1}:")
+
+# marksheet() 
+# print("_________________________________________________")
+# print("YOUR 3RD SEMESTER RESULT :")  
+# print(f"Your marks are{marks}")
+# print("_________________________________________________")
+
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# PROGRAM TO FIND CUBES USING DICTIONARY:-
+# n=int(input("Enter the number upto which you want's cube"))
+# def cube(n) :
+#  cube={}
+#  for i in range(0,n+1) : 
+#   cube[i] = i**3
+#  return cube
+# print(cube(n))     
+
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# PROGRAM TO USE LIST COMPREHENTION :-
+# IF NUMBER IS ODD CHANGE TO NEGATIVE ELSE SQARE IT:-
+# n= int(input("Enter the number upto which you wants your program:")) 
+# def lc(n):
+#     return[i**2 if (i%2==0)  else(-i) for i in range(0,n+1)
+#       print(lc(n))
+  
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# 
+# s={1,2,3,4,3,2,1}
+# l=[1,2,3,4,3,2,1]
+# print(s) #set
+# print(l) # list
+# print(list(set(l))) # conversion of list to set to list for deleting duplication of list .
+
